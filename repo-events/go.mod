8f68e4cdfba77e990c8e8c5208f10671ed3afcf1
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-11-29 11:42:00 +0000

    Support only issue comments
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- repo-events/go.mod
+++ repo-events/go.mod
@@ -2,4 +2,7 @@ module github.com/openfaas/repo-events/repo-events
 
 go 1.16
 
-require github.com/openfaas/templates-sdk v0.0.0-20200723110415-a699ec277c12
+require (
+	github.com/google/go-github/v40 v40.0.0
+	github.com/openfaas/templates-sdk v0.0.0-20200723110415-a699ec277c12
+)
--- repo-events/handler.go
+++ repo-events/handler.go
@@ -1,20 +1,59 @@
 package function
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/google/go-github/v40/github" // with go modules enabled (GO111MODULE=on or outside GOPATH)
 	handler "github.com/openfaas/templates-sdk/go-http"
 )
 
 // Handle a function invocation
 func Handle(req handler.Request) (handler.Response, error) {
-	var err error
 
-	message := fmt.Sprintf("Body: %s", string(req.Body))
+	webhookSecretKey, err := os.ReadFile("/var/openfaas/secrets/webhook-secret")
+	if err != nil {
+		return handler.Response{
+			StatusCode: http.StatusInternalServerError,
+			Body:       []byte(fmt.Sprintf("Error reading webhook secret: %s", err)),
+		}, err
+	}
+
+	payload, err := github.ValidatePayloadFromBody(req.Header.Get("Content-Type"),
+		bytes.NewBuffer(req.Body),
+		req.Header.Get(github.SHA256SignatureHeader),
+		webhookSecretKey)
+
+	if err != nil {
+		return handler.Response{
+			StatusCode: http.StatusBadRequest,
+			Body:       []byte(fmt.Sprintf("Error validating payload: %s", err.Error())),
+		}, err
+	}
+
+	eventType := req.Header.Get(github.EventTypeHeader)
+	event, err := github.ParseWebHook(req.Header.Get(eventType), payload)
+	if err != nil {
+		return handler.Response{
+			StatusCode: http.StatusBadRequest,
+			Body:       []byte(fmt.Sprintf("Error parsing webhook: %s", err.Error())),
+		}, err
+	}
+
+	switch event := event.(type) {
+	case *github.IssueCommentEvent:
+		fmt.Printf("Issue comment body: %s\n", event.GetComment().GetBody())
+	default:
+		return handler.Response{
+			StatusCode: http.StatusBadRequest,
+			Body:       []byte(fmt.Sprintf("Event type not supported: %s", eventType)),
+		}, nil
+	}
 
 	return handler.Response{
-		Body:       []byte(message),
-		StatusCode: http.StatusOK,
+		Body:       []byte("Accepted webhook"),
+		StatusCode: http.StatusAccepted,
 	}, err
 }
